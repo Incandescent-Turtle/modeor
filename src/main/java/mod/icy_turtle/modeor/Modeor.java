1a9b5a20f3aab60f0693443889f2a34ace02fe5d
Rory Macdonald <rorsmacd@gmail.com>
2025-10-04 22:56:18 -0700

    initial commit

---
+++ src/main/java/mod/icy_turtle/modeor/Modeor.java
@@ -0,0 +1,12 @@
+package mod.icy_turtle.modeor;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Modeor implements ModInitializer
+{
+
+	@Override
+	public void onInitialize()
+	{
+	}
+}
---
+++ src/main/java/mod/icy_turtle/modeor/client/ModeorClient.java
@@ -0,0 +1,12 @@
+package mod.icy_turtle.modeor.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModeorClient implements ClientModInitializer
+{
+
+	@Override
+	public void onInitializeClient()
+	{
+	}
+}
