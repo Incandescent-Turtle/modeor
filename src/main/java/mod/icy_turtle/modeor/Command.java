4dd3f5e70f7de4797481a5f7b4f8ef43b099adf3
James Shapas <j@jmba.local>
2025-10-05 02:11:27 -0700

    add args to command and summon entity

--- src/main/java/mod/icy_turtle/modeor/Command.java
+++ src/main/java/mod/icy_turtle/modeor/Command.java
@@ -1,12 +1,36 @@
 package mod.icy_turtle.modeor;
 
+import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.context.CommandContext;
+import mod.icy_turtle.modeor.entity.MeteorEntity;
 import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
 
 public class Command {
     public static int executeCommand(CommandContext<ServerCommandSource> context) {
-        context.getSource().sendFeedback(() -> Text.literal("Modeor"), true);
+
+        ServerCommandSource src = context.getSource();
+
+        src.sendFeedback(() -> Text.literal(
+                String.valueOf(IntegerArgumentType.getInteger(context, "mass"))), true);
+        src.sendFeedback(() -> Text.literal(
+                String.valueOf(IntegerArgumentType.getInteger(context, "angle"))), true);
+        src.sendFeedback(() -> Text.literal(
+                String.valueOf(IntegerArgumentType.getInteger(context, "speed"))), true);
+
+        ServerPlayerEntity player = src.getPlayer();
+
+        MeteorEntity meteor = new MeteorEntity(ModEntities.METEOR, src.getWorld());
+
+        assert player != null;
+        // todo probably check what direction player is facing to always spawn it in their view
+        // player.getYaw() -> left and right
+        // player.getPitch() -> up and down
+
+        meteor.setPosition(player.getX() + 10, player.getY() + 20, player.getZ());
+
+        src.getWorld().spawnEntity(meteor);
         return 1;
     }
 }
--- src/main/java/mod/icy_turtle/modeor/Modeor.java
+++ src/main/java/mod/icy_turtle/modeor/Modeor.java
@@ -1,21 +1,25 @@
 package mod.icy_turtle.modeor;
 
+import com.mojang.brigadier.arguments.IntegerArgumentType;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.minecraft.server.command.CommandManager;
-import net.minecraft.server.command.ServerCommandSource;
+
+import static net.minecraft.server.command.CommandManager.argument;
 
 public class Modeor implements ModInitializer
 {
-	public static final String MOD_ID = "modeor";
+    public static final String MOD_ID = "modeor";
 
-	@Override
-	public void onInitialize()
-	{
+    @Override
+    public void onInitialize()
+    {
         CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
-            dispatcher.register(CommandManager.literal("modeor")
-                    .requires(ServerCommandSource::isExecutedByPlayer)
-                            .executes(Command::executeCommand));
+            dispatcher.register(CommandManager.literal("meteor")
+                    .then(argument("mass", IntegerArgumentType.integer())
+                            .then(argument("angle", IntegerArgumentType.integer())
+                                    .then(argument("speed", IntegerArgumentType.integer())
+                                            .executes(Command::executeCommand)))));
         });
     }
 }
